3027d6fea189b45fe2f63000c2e3da94a0837141
pan.liangp <pan.liangp@alibaba-inc.com>
2017-10-13 17:06:30 +0800

    delete *.o

--- src/release.h
+++ src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "a6bdbae0"
+#define REDIS_GIT_SHA1 "07518063"
 #define REDIS_GIT_DIRTY "0"
-#define REDIS_BUILD_ID "r10f07456.sqa.zmf-1507885151"
+#define REDIS_BUILD_ID "r10f07456.sqa.zmf-1507885563"
